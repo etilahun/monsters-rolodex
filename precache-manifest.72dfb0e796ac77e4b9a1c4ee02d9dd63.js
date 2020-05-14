039573a0331d17a17ffbde7b80578ac3a7681066
ET <elaykasa@yahoo.com>
2020-05-14 16:23:25 -0400

    Updates

---
+++ precache-manifest.72dfb0e796ac77e4b9a1c4ee02d9dd63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e603ab5b233134eab86243b31ae8ed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e70fd295cbe7d539c213",
+    "url": "/monsters-rolodex/static/css/main.c2cd7399.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e70fd295cbe7d539c213",
+    "url": "/monsters-rolodex/static/js/main.156bcdfd.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
